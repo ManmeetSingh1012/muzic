881ecf951166376142415dabb6d50a4c369acba7
ManmeetSingh1012 <manmeetsinghnss@gmail.com>
2024-09-06 15:55:15 +0530

    new changes

--- app/api/stream/route.ts
+++ app/api/stream/route.ts
@@ -2,6 +2,8 @@ import { NextRequest, NextResponse } from "next/server";
 import { z } from "zod";
 import { prismaClient } from "@/app/lib/db";
 import { useRouter } from "next/navigation";
+//@ts-ignore
+import youtubesearchapi from "youtube-search-api";
 
 const yt_regex = new RegExp(
   "^(http(s)?://)?((w){3}.)?youtu(be|.be)?(.com)?/.+"
@@ -30,6 +32,17 @@ export async function POST(req: NextResponse) {
     }
 
     const extractId = data.url.split("v=")[1];
+    const res = await youtubesearchapi.GetVideoDetails(extractId);
+    console.log(res.title);
+    console.log(res.thumbnail);
+    // console.log(JSON.stringify(res.thumbnails.thumbnails))
+    //console.log(res)
+
+
+    const thumbnail = res.thumbnail.thumbnails
+    thumbnail.sort((a:{width:number}, b:{width:number}) => a.width < b.width ? -1 : 1)
+    console.log(thumbnail)
+
 
     await prismaClient.stream.create({
       data: {
@@ -37,6 +50,11 @@ export async function POST(req: NextResponse) {
         url: data.url,
         extractedId: extractId,
         type: "YOUTUBE",
+        title: res.title ?? "No title",
+        smallImg : thumbnail[0].url ?? "No image",
+        bigImg : thumbnail[thumbnail.length - 1].url ?? "No image",
+
+
       },
     });
 
---
+++ test.js
@@ -0,0 +1,4 @@
+let index = 0 
+let str = "java"
+let c = str.at(--index)
+console.log(c.length) // a
